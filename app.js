1e38d00e002a5929f1f9142e39ca569a2b531ea6
Pranjal Uniyal <pranjaluniyal26@gmail.com>
2025-07-13 19:20:47 +0530

    own update

--- app.js
+++ app.js
@@ -1,145 +1,372 @@
-//JAVASCRIPT
+const GameBoard = (function () {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
 
-// Game State
-const players = ["O", "X"];
-const gameBoard = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer;
-let gameBoardElem;
+  const updateGameboard = (place, mark, name1, name2) => {
+    gameboard[place - 1] = mark;
+    return displayController.winDisplay(name1, name2);
+  };
 
-// Element creation
-const createTitle = (title) => {
-  const titleElem = document.createElement("h1");
+  const resetGameboard = () => {
+    gameboard = ["", "", "", "", "", "", "", "", ""];
+  };
 
-  titleElem.textContent = title;
+  const checkForWin = () => {
+    const arrX = [];
+    const arrO = [];
 
-  document.body.appendChild(titleElem);
-};
-
-const makeGameBoardElem = () => {
-  const gameBoardElem = document.createElement("div");
-
-  gameBoardElem.classList.add("game-board");
-
-  return gameBoardElem;
-};
-
-const makeSquareElem = (squareNumber) => {
-  const squareElement = document.createElement("div");
-  squareElement.classList.add("game-square");
-
-  squareElement.addEventListener(
-    "click",
-    (event) => {
-      const { target } = event;
-      target.textContent = currentPlayer;
-      gameBoard[squareNumber] = currentPlayer;
-      checkBoard();
-      switchPlayer();
-    },
-    { once: true }
-  );
-  return squareElement;
-};
-
-// Game Control
-const switchPlayer = () => {
-  if (currentPlayer === players[0]) {
-    currentPlayer = players[1];
-  } else {
-    currentPlayer = players[0];
-  }
-};
-
-const checkBoard = () => {
-  // gameBoard
-  // ['0', '1', '2']
-  // ['3', '4', '5']
-  // ['6', '7', '8']
-  const winningStates = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let winState of winningStates) {
-    const [position1, position2, position3] = winState;
-    if (
-      gameBoard[position1] !== "" &&
-      gameBoard[position1] === gameBoard[position2] &&
-      gameBoard[position1] === gameBoard[position3]
+    for (let i = 0; i < gameboard.length; i++) {
+      mark = gameboard[i];
+      if (mark === "X") {
+        arrX.push(i + 1);
+      } else if (mark === "O") {
+        arrO.push(i + 1);
+      }
+    }
+    if (arrX.length >= 3 && arrayCheck(arrX)) {
+      return "X";
+    } else if (arrO.length >= 3 && arrayCheck(arrO)) {
+      return "O";
+    } else if (
+      (arrX.length === 4 && arrO.length === 5 && arrayCheck(arrO) === false) ||
+      (arrO.length === 4 && arrX.length === 5 && arrayCheck(arrX) === false)
     ) {
-      completeGame(`${gameBoard[position1]} player Wins!`);
+      return "tie";
+    } else {
+      return "in progress";
+    }
+  };
+
+  return { resetGameboard, updateGameboard, checkForWin };
+})();
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+function playerCombination(arr) {
+  let allPlayerCombination = [];
+  for (let i = 0; i < arr.length; i++) {
+    let temp = [];
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        temp.push(arr[i], arr[j], arr[k]);
+
+        allPlayerCombination.push(temp);
+        temp = [];
+      }
     }
   }
 
-  const allSquaresUsed = gameBoard.every((square) => square !== "");
+  return allPlayerCombination;
+}
 
-  if (allSquaresUsed) {
-    completeGame(`It's a draw!`);
-  }
-};
-
-const completeGame = (message) => {
-  const overlayElem = document.createElement("div");
-  overlayElem.style.position = "fixed";
-  overlayElem.style.top = "0";
-  overlayElem.style.left = "0";
-  overlayElem.style.bottom = "0";
-  overlayElem.style.right = "0";
-  overlayElem.style.backgroundColor = "rgba(0,0,0,0.8)";
-  overlayElem.style.display = "flex";
-  overlayElem.style.flexDirection = "column";
-  overlayElem.style.justifyContent = "center";
-  overlayElem.style.alignItems = "center";
-  overlayElem.style.textAlign = "center";
-
-  const messageElem = document.createElement("h2");
-  messageElem.textContent = message;
-  messageElem.style.color = "white";
-  messageElem.style.fontSize = "100px";
-
-  overlayElem.appendChild(messageElem);
-
-  const restartButtonElem = document.createElement("button");
-  restartButtonElem.textContent = "Restart";
-  restartButtonElem.style.backgroundColor = "transparent";
-  restartButtonElem.style.color = "white";
-  restartButtonElem.style.border = "1px solid white";
-  restartButtonElem.style.padding = "10px 30px";
-
-  restartButtonElem.addEventListener("click", () => {
-    resetGame();
-    document.body.removeChild(overlayElem);
-  });
-
-  overlayElem.appendChild(restartButtonElem);
-
-  document.body.appendChild(overlayElem);
-};
-
-// Initalisation
-const resetGame = () => {
-  if (gameBoardElem) {
-    // Old game board exists
-    document.body.removeChild(gameBoardElem);
+function arrayCheck(arr) {
+  const winningCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  const playerCombinations = playerCombination(arr);
+  for (let i = 0; i < playerCombinations.length; i++) {
+    let combo = playerCombinations[i];
+    for (let j = 0; j < winningCombinations.length; j++) {
+      let win = winningCombinations[j];
+      if (arraysEqual(win, combo)) {
+        return true;
+      }
+    }
   }
 
-  gameBoardElem = makeGameBoardElem();
+  return false;
+}
 
-  for (let row = 0; row < 9; row++) {
-    gameBoardElem.appendChild(makeSquareElem(row));
-  }
+function Player(name, mark) {
+  return { name, mark };
+}
+const displayController = (function () {
+  const gameDisplay = (Player1, Player2) => {
+    Game.flow = "continue";
+    let turnCount = 0;
+    const squareList = document.querySelectorAll(".square");
+    squareList.forEach((square) => {
+      square.addEventListener("click", () => {
+        if (!square.firstChild) {
+          const name = square.id;
+          const marker = document.createElement("h1");
+          marker.classList.add("mark");
+          if (turnCount % 2 === 0) {
+            turnCount++;
+            marker.textContent = "×";
+            square.appendChild(marker);
+            switch (name) {
+              case "One":
+                if (Game.flow === "continue") {
+                  Game.flow = GameBoard.updateGameboard(
+                    1,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
+                }
+                break;
+              case "Two":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    2,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
 
-  currentPlayer = players[0];
-  gameBoard.fill("");
+                break;
+              case "Three":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    3,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
 
-  document.body.appendChild(gameBoardElem);
-};
+                break;
+              case "Four":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    4,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
 
-createTitle("Tic-tac-toe");
-resetGame();
+                break;
+              case "Five":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    5,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Six":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    6,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Seven":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    7,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Eight":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    8,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Nine":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    9,
+                    "X",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              default:
+            }
+          } else {
+            turnCount++;
+            marker.textContent = "○";
+            square.appendChild(marker);
+            switch (name) {
+              case "One":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    1,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+                break;
+              case "Two":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    2,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Three":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    3,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Four":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    4,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Five":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    5,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Six":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    6,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Seven":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    7,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Eight":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    8,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              case "Nine":
+                if (Game.flow === "continue")
+                  Game.flow = GameBoard.updateGameboard(
+                    9,
+                    "O",
+                    Player1.name,
+                    Player2.name
+                  );
+
+                break;
+              default:
+            }
+          }
+        }
+      });
+    });
+  };
+
+  const resetDisplay = () => {
+    const statusReset = document.querySelector(".result");
+    statusReset.textContent = ``;
+
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+      if (square.firstChild) square.removeChild(square.firstChild);
+    });
+  };
+  const winDisplay = (name1, name2) => {
+    const result = document.querySelector(".result");
+    if (GameBoard.checkForWin() === "X") {
+      result.textContent = `${name1} wins!`;
+      return "over";
+    } else if (GameBoard.checkForWin() === "O") {
+      result.textContent = `${name2} wins!`;
+      return "over";
+    } else if (GameBoard.checkForWin() === "tie") {
+      result.textContent = "Tie";
+      return "over";
+    } else {
+      result.textContent = "In Progress";
+      return "continue";
+    }
+  };
+
+  return { gameDisplay, resetDisplay, winDisplay };
+})();
+
+function Player(name, mark) {
+  return { name, mark };
+}
+
+const Game = (function () {
+  let flow = "continue";
+  const gameStart = () => {
+    let player1, player2;
+    const formBtn = document.querySelector("#player-form");
+    formBtn.addEventListener("submit", (event) => {
+      event.preventDefault();
+      player1 = document.querySelector("#player-1").value;
+      player2 = document.querySelector("#player-2").value;
+      const showList = document.querySelectorAll(".hidden");
+      showList.forEach((item) => {
+        item.classList.remove("hidden");
+      });
+
+      playerOne = Player(player1, "X");
+      playerTwo = Player(player2, "O");
+      displayController.gameDisplay(playerOne, playerTwo);
+    });
+
+    const resetBtn = document.querySelector("#reset");
+    resetBtn.addEventListener("click", () => {
+      displayController.resetDisplay();
+      GameBoard.resetGameboard();
+      flow = "continue";
+      displayController.gameDisplay(playerOne, playerTwo);
+    });
+  };
+
+  return { gameStart, flow };
+})();
+
+Game.gameStart();
